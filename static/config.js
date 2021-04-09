27f952ab4a9af5b0c1ecb3ed64d656c09eddc8f7
FrankYeah <aaa24295234@gmail.com>
2021-04-09 13:51:46 +0800

    first commit

---
+++ static/config.js
@@ -0,0 +1,4 @@
+window.env = window.env || {}
+window.env.PUBLIC_API_ROOT_URL_IMG = 'http://192.168.66.79:80/'
+// window.env.PUBLIC_API_ROOT_URL = 'http://192.168.66.79:8083/'
+window.env.PUBLIC_API_ROOT_URL = 'https://api.reborn.in/'
---
+++ static/js/0.c4e0e7c43a00b685b99b.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{FA7F:function(e,o,p){e.exports=p.p+"static/img/hello-emoji.5dead0f.png"}});
+//# sourceMappingURL=0.c4e0e7c43a00b685b99b.js.map
